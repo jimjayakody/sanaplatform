d8caf267793b88fb6d526fb2b9712fa14ab80d0e
jimjayakody <imjayakody@gmail.com>
2025-04-07 19:42:28 +0530

    Remove Org home

--- spartify/e2e/spartify_home.cy.ts
+++ spartify/e2e/spartify_home.cy.ts
@@ -7,23 +7,5 @@ spartiSuite(
   "OrangeHRM Home Dashboard - Test Suite",
   { tags: ["@reg", "@home"] },
   () => {
-    it("Test - Verify Profile Logout", { tags: "@smk" }, () => {
-      loginPage
-        .visitUrl()
-        .step_enterUsername("Admin")
-        .step_enterPassword("admin123")
-        .step_clickLogin()
-        .step_profileOption("Logout");
-    });
-
-    it("Test - Verify Given Menu Search Selection", { tags: "@smk" }, () => {
-      loginPage
-        .visitUrl()
-        .step_enterUsername("Admin")
-        .step_enterPassword("admin123")
-        .step_clickLogin()
-        .step_searchOption("Leave")
-        .step_profileOption("Logout");
-    });
   }
 );
